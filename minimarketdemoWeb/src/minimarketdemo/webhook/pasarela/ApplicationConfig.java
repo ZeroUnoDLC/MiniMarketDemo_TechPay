b04b971fcea0e9995cdc328fc0e4b475196b881b
ZeroUnoDLC <bodelacruze@utn.edu.ec>
2025-02-01 13:28:38 -0500

    Creación de un endpoint para que MInimarketDemo pueda ser capaz de recibir solicitudes POST

---
+++ minimarketdemoWeb/src/minimarketdemo/webhook/pasarela/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package minimarketdemo.webhook.pasarela;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+// Configura el prefijo para las rutas de tu API
+@ApplicationPath("/api")
+public class ApplicationConfig extends Application {
+}
